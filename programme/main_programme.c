22f11a2ee7594b6b3e1aa2b1f3d1c83e03f28b92
CLAUDEL Corentin p2104031 <corentin.claudel@etu.univ-lyon1.fr>
2021-11-29 13:03:39 +0100

    transformation CSV en Tableau mis dans le main

--- programme/main_programme.c
+++ programme/main_programme.c
@@ -2,12 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mes_fonctions.h"
+#define chemin "../test_a_detruire.csv"
 #define ligne 6000
 #define colonne 7
+#define MAX_LENGTH 5000
 
 int main(void)
 {
+    FILE *fichier=fopen(chemin,"r");
+
+    if (fichier==NULL)
+    {
+        printf("Echec ouverture fichier\n");
+        return EXIT_FAILURE;
+    }
+    char tableau_CSV[ligne][colonne];
+    int ajout_colonne=0;
+    int ajout_ligne=0;
+    while (ajout_ligne<ligne)
+    {
+        while (ajout_colonne<colonne)
+        {
+            tableau_CSV[ajout_ligne][ajout_colonne]=fgets(ligne,MAX_LENGTH,fichier);
+        }
+    }
+    return EXIT_SUCCESS;
     menu();
-    printf("Au revoir !")
+    printf("Au revoir !");
     return 0;
 }
--- programme/mes_fonctions.c
+++ programme/mes_fonctions.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define chemin "../test_a_detruire.csv"
-#define MAX_LENGTH 5000
+
 
 void menu(void)
 {
@@ -28,53 +27,6 @@ void menu(void)
         }
         else
         {
-            printf("Commande non valide")
+            printf("Commande non valide");
         }
-}
-
-int test_fichier(void)
-{
-    FILE *fichier=fopen(chemin,"r");
-
-    if (fichier==NULL)
-    {
-        printf("Echec ouverture fichier\n");
-        return EXIT_FAILURE;
-    }
-    return EXIT_SUCCESS;
-}
-
-int lecture(void)
-{
-    if (test_fichier()==EXIT_FAILURE)
-        return EXIT_FAILURE;
-
-    char ligne[MAX_LENGTH];
-    char *token; //Contiendra les différents champ de chaque ligne après tokenization
-    FILE *fichier=fopen(chemin,"r");
-
-    while (fgets(ligne,MAX_LENGTH,fichier))
-    {
-        printf("Ligne : %s", ligne);
-        token = strtok(ligne, ",");
-        while (token != NULL)
-        {
-            printf("Token : %s\n", token);
-            token =strtok(NULL, ",");
-        }
-    }
-
-    printf("\n");
-    fclose(fichier);
-    return EXIT_SUCCESS;
-}
-
-int transformation_CSV_tableau(int ligne, int colonne)
-{
-    if (test_fichier()==EXIT_FAILURE)
-        return EXIT_FAILURE;
-    else
-    {
-        
-    }    
 }
