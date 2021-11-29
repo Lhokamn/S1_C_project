badf1c2774bf1cb8fea5fd217887445e66323b3b
MASSARD VALENTIN p2103668 <valentin.massard@etu.univ-lyon1.fr>
2021-11-29 12:07:17 +0000

    Replace main_programme.c Valentin 13:06 29/11/2021

--- programme/main_programme.c
+++ programme/main_programme.c
@@ -5,6 +5,18 @@
 
 int main(void)
 {
-    lecture();
+    char choix;
+    printf("Que voulez-vous faire ? Choisissez un nombre \n 1.Lire le tableau \n 2.Modifier le fichier");
+    switch(scanf("%c",&choix)) {
+
+        case choix='1':
+            lecture();
+        break;
+        case choix ='2':
+
+        break;
+        default:
+        printf("Erreur: Fin système");
+    }
     return 0;
 }
